af7cb3029e45422517919a591583daa0f5401e11
henrique fan <fanhenrique@gmai.com>
2021-06-22 14:38:05 -0300

    update query students

--- queryStudents.py
+++ queryStudents.py
@@ -12,9 +12,8 @@ def main():
 
 	parser = argparse.ArgumentParser(description='Query students')
 
-	parser.add_argument('--course', '-c', help='Curso', required=True, type=str)
-	# parser.add_argument('--quiz', '-q', help='Questionário', required=True, type=str)
 	parser.add_argument('--project', '-p', help='Diretório do projeto', required=True, type=str)
+	parser.add_argument('--course', '-c', help='Curso', required=True, type=str)
 	
 	help_msg = "Logging level (INFO=%d DEBUG=%d)" % (logging.INFO, logging.DEBUG)
 	parser.add_argument("--log", "-l", help=help_msg, default=DEFAULT_LOG_LEVEL, type=int)
@@ -27,17 +26,14 @@ def main():
 		logging.basicConfig(format='%(asctime)s.%(msecs)03d %(message)s', datefmt=TIME_FORMAT, level=args.log)
 
 
+	#Conexão banco de dados
 	cnx = mysql.connector.connect(host='127.0.0.1', user='amcmoodle', database='moodle', password='amcmoodle')
 	cursor = cnx.cursor()
+
 	
 	logging.info('Connected to the databese')
-
-	# cursor.execute('SELECT id, shortname FROM mdl_course WHERE shortname=\''+args.course+'\';')
-	# for id, shortname in cursor:
-	# 	id_course = id
-	# print(id_course)
-
 	logging.info('Search for course students')
+
 
 	cmd = '''
 	SELECT mdl_user_enrolments.enrolid, 
@@ -48,35 +44,36 @@ def main():
 	mdl_user.firstname, 
 	mdl_user.lastname, 
 	mdl_user.email 
-	FROM mdl_user_enrolments, 
+	FROM 
+	mdl_user_enrolments, 
 	mdl_enrol, 
 	mdl_course, 
 	mdl_user 
-	WHERE mdl_enrol.id=mdl_user_enrolments.enrolid AND 
+	WHERE 
+	mdl_enrol.id=mdl_user_enrolments.enrolid AND 
 	mdl_user_enrolments.userid=mdl_user.id AND 
 	mdl_enrol.courseid=mdl_course.id AND 
 	mdl_course.shortname=\''''+args.course+'''\';
 	'''
 	cursor.execute(cmd)
 
+
 	logging.info('mysql: ' + cmd)
 
+
+	# Cria arquivo com a lista dos alunos
 	with open(args.project + '/list.csv', 'w') as file:
 		
 		file.write('name,forename,id\n')
 		
 		for(enrolid, userid, courseid , courseName, userName, firstName, lastName, email) in cursor:
 			# print(enrolid, userid, courseid, courseName, userName, firstName, lastName, email)
-
 			file.write(firstName + ',' + lastName + ',' + str(userid) + '\n')
-		
 
 	logging.info('File created list.csv\n')
 	
 
-	# cursor.execute('SELECT id, course, name FROM mdl_quiz WHERE course=\''+str(id_course)+'\' AND name=\''+args.quiz+'\';')
-	# for id, course, name in cursor:
-	# 	id_quiz = id
+	
 
 
 
